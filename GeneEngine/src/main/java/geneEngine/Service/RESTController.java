cde3a2c3ae26abce663f499c7e12600ccfb0d8c7
Yuichiro021 <71137269+CristianCmeciu@users.noreply.github.com>
2025-05-09 01:07:32 +0300

    Removed CORS

--- GeneEngine/src/main/java/geneEngine/Service/RESTController.java
+++ GeneEngine/src/main/java/geneEngine/Service/RESTController.java
@@ -1,11 +1,9 @@
 package geneEngine.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class RESTController {
